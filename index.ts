71e99ca13218fbbf3dbabd4f7105318d22e5c45d
Asadkhan12334455 <160310609+Asadkhan12334455@users.noreply.github.com>
2024-04-01 18:45:34 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todo = [];
+let condition = true;
+
+while (condition) {
+  let addTask = await inquirer.prompt([
+    {
+      name: "todo",
+      type: "input",
+      message: chalk.green("What do you want to add in Your ToDo List?"),
+    },
+    {
+      name: "addMore",
+      type: "confirm",
+      message: chalk.greenBright("Do you want to add more?"),
+      default: "false",
+    },
+  ]);
+  todo.push(addTask.todo);
+  condition = addTask.addMore;
+  console.log(todo);
+}
+let clearList = await inquirer.prompt([
+  {
+    name: "clear",
+    type: "confirm",
+    message: chalk.greenBright("Do you want to clear your ToDo list?"),
+    default: false,
+  },
+]);
+
+if (clearList.clear) {
+  todo = [];
+  console.log(chalk.yellow("ToDo list cleared!"));
+} else {
+  console.log(chalk.yellow("ToDo list not cleared!"));
+}
+
+console.log(chalk.yellow("Final ToDo list:"));
+console.log(todo);
